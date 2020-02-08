0f7e0e149d2d1bb81d14cd3db525eece1c235b50
Vinícius Pacheco Vieira <vinipachecov@gmail.com>
2020-02-07 22:03:21 -0300

    feat: esnsure sIGNupController retutnrs 400 if no password is provided"

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,15 +3,11 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helper/http-helpers'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password']
     for (const field of requiredFields) {
-      return badRequest(new MissingParamError(field))
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
