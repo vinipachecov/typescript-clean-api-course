3ee61fd71077af1be5a70793c110bb52947e043f
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-06-09 19:19:47 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -114,4 +114,9 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+  test('Should return null if TokenGenerator returns null', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    await expect(accessToken).toBe('any_token')
+  })
 })
