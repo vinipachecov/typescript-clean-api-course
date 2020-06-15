b708e93647ac4bad68abe9030c7c5609ddf1d7e9
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-06-15 12:05:37 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -48,5 +48,14 @@ describe('Login Middleware', () => {
         })
         .expect(200)
     })
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'vinicius@mail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
